5a49c2813b447fe02caae3b2a4dc8d4945ab2801
chomusukei <42434929+chomusukei@users.noreply.github.com>
2018-08-19 23:19:19 +0800

    Create OOP.java

---
+++ OOP.java
@@ -0,0 +1,20 @@
+package Main;
+
+import Array.LinkList;
+import Logging.Logging;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import Frame.MainFrame;
+
+//This will be the main
+
+class OOP
+{
+    public static void main(String[] args) throws Exception 
+    {              
+        MainFrame frame = new MainFrame();
+        
+        frame.mainW();
+        frame.testrun();
+    } 
+}
